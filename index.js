c74ecb964cdf9f126e58f82d821cba0296861f60
Tory Miell <torymiell@Torys-MacBook-Pro.local>
2026-09-23 15:26:46 +0200

    Route client registration notifications to hello@svecchayoga.com

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
    require("dotenv").config();
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const fetch = (...args) => import("node-fetch").then(({ default: f }) => f(...args));
